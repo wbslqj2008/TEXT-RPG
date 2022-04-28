7c94f3ea910f5fef931fa3aadce06c6357275084
wbslqj2008 <wbslqj2008@naver.com>
2022-04-28 16:31:27 +0900

    초기 커밋.

---
+++ Text RPG/Creature.cs
@@ -0,0 +1,44 @@
+﻿namespace TextRPG
+{
+    public enum CreatureType
+    {
+        None,
+        Player = 1,
+        Monster = 2
+    }
+    class Creature
+    {
+        CreatureType type;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+        public int GetHP()
+        {
+            return hp;
+        }
+        public int GetAttack()
+        {
+            return attack;
+        }
+        public bool IsDead()
+        {
+            return hp <= 0;
+        }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+    }
+}
---
+++ Text RPG/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Knight();
+            Player player2 = new Archer();
+            Monster monster = new Orc();
+
+            int damage = player.GetAttack();
+            player2.OnDamaged(damage);
+        }
+    }
+}
